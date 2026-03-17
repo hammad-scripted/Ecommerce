45ecdf3b67e80ece7a76e88e33aac63be5f703ab
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-03-17 23:01:33 +0530

    uniqueConstraint

--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -5,31 +5,53 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")})
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     @NotBlank
-    @Size(max=25)
+    @Size(max = 25)
+    @Column(name = "username")
     private String userName;
+
+
     @NotBlank
     @Size(max = 40)
     @Email
+   @Column(name = "email")
     private String email;
+
+
     @NotBlank
     @Size(max = 30)
+    @Column(name = "password")
     private String password;
 
-    public User(String userName,String password,String email) {
-        this.userName=userName;
-        this.email=email;
-        this.password=password;
+    public User(String userName, String password, String email) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
     }
+
+    @Setter
+    @Getter
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
 }
